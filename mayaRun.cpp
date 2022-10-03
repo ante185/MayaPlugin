c33a1347ec1926d6a5be7f4873881b194c1bbf12
ante <49555217+ante185@users.noreply.github.com>
2022-10-03 14:27:52 +0200

    updated mayaRun.cpp and git ignored properties

--- mayaRun.cpp
+++ mayaRun.cpp
@@ -7,7 +7,6 @@
 #include <unordered_map>
 
 using namespace std;
-//MCallbackIdArray callbackIdArray;
 unordered_map<string, MCallbackId> callbacks;
 MObject m_node;
 MStatus status = MS::kSuccess;
@@ -17,42 +16,51 @@ enum NODE_TYPE { TRANSFORM, MESH };
 MTimer gTimer;
 // keep track of created meshes to maintain them
 queue<MObject> newMeshes;
+static double totaltime = 0.0;
 
 
-void nodeAttributeChanged(MNodeMessage::AttributeMessage msg, MPlug &plug, MPlug &otherPlug, void* x)
+void nodeAttributeChanged(MNodeMessage::AttributeMessage msg, MPlug& plug, MPlug& otherPlug, void* x)
 {
-	//if (msg & MNodeMessage::AttributeMessage::kConnectionMade) {
-	//	if (plug.node().hasFn(MFn::kTransform)) {
-	//		MFnTransform firstNode(plug.node(), &status);
-	//		if (status == MS::kSuccess) {
-	//			cout << "First - we are here: " << firstNode.name() << endl;
-	//			MPlugArray connections;
-	//			MPlug firstNodePlugs = firstNode.findPlug("translate", &status);
-	//			if (status == MS::kSuccess) {
-	//				firstNodePlugs.connectedTo(connections, true, false);
-	//			}
-	//			for (size_t i = 0; i < connections.length(); i++) {
-	//				if (connections[i].node().hasFn(MFn::kTransform)) {
-	//					MFnTransform secondNode(connections[i].node(), &status);
-	//					if (status == MS::kSuccess) {
-	//						cout << "Now - we are here: " << secondNode.name() << endl;
-	//					}
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
-
-	if (plug.node().hasFn(MFn::kMesh)) {
+	if (msg & MNodeMessage::AttributeMessage::kAttributeEval) {
 		MFnMesh mesh(plug.node());
-		if (mesh.name().length() > 0) {
-			cout << "Modified: " << mesh.name() << endl;
-			MPointArray pointArray;
-			mesh.getPoints(pointArray, MSpace::kObject);
-			cout << pointArray << endl;
+		cout << "Modified: " + mesh.name() << endl;
+		MPointArray pointArray;
+		mesh.getPoints(pointArray, MSpace::kObject);
+		cout << pointArray << endl;
+	}
+}
+void topoChanged(MObject& node, void* data) {
+	MFnDependencyNode dn(node);
+	auto cbID = MNodeMessage::addAttributeChangedCallback(node, nodeAttributeChanged, NULL, &status);
+	if (status == MS::kSuccess) {
+		string name = dn.name().asChar();
+		string suffix = "AttributeChanged";
+		auto itr = callbacks.find(name + suffix);
+		if (itr != callbacks.end()) {
+			cout << "Callback erased: " + itr->first + "\n";
+			MMessage::removeCallback(itr->second);
+			callbacks[name + suffix] = cbID;
+		}
+		else {
+			callbacks.insert({ name + suffix, cbID });
 		}
 	}
+	else {
+		cout << "Error inserting callback: " + dn.name() + "\n";
+	}
+}
 
+void nodeTransformChanged(MNodeMessage::AttributeMessage msg, MPlug& plug, MPlug& otherPlug, void* userdata) {
+	if (msg & MNodeMessage::AttributeMessage::kAttributeSet) {
+
+		MFnTransform tform(plug.node());
+		auto transform = tform.transformation();
+		cout << "Local transform for " << tform.name() << ": " << transform.asMatrix() << endl << endl;
+		MDagPath test1;
+		tform.getPath(test1);
+		auto worldMtrx = (test1.inclusiveMatrix());
+		cout << "Global transform for " << tform.name() << ": " << worldMtrx << endl << endl;
+	}
 }
 
 
@@ -61,21 +69,24 @@ void nodeNameChanged(MObject& node, const MString& str, void* clientData)
 	if (node.hasFn(MFn::kDependencyNode)) {
 		MFnDependencyNode dn(node);
 		if (status == MS::kSuccess /*&& strcmp(str.asChar(), dn.name().asChar()) != 0*/) {
-			cout << "Node " + str + " has been renamed " + dn.name() << endl;
+			cout << "Node " + str + " has been renamed to " + dn.name() << endl;
 		}
 	}
 }
 
 void dirtyNode(MObject& node, void* clientData)
 {
-	if (node.hasFn(MFn::kTransform)) {
-		MFnTransform tform(node);
-		//cout << tform.name() << " suspects their transform is changed" << endl;
-		auto transform = tform.transformation();
-		if (status == MS::kSuccess) {
-			cout << "Local transform: " << transform.asMatrix() << endl << endl;
-		}
-	}
+	//if (node.hasFn(MFn::kTransform)) {
+	//	MFnTransform tform(node);
+	//	//cout << tform.name() << " suspects their transform is changed" << endl;
+	//	auto transform = tform.transformation();
+	//	cout << "Local transform for " << tform.name() << ": " << transform.asMatrix() << endl << endl;
+	//	MDagPath test1;
+	//	tform.getPath(test1);
+	//	auto worldMtrx = (test1.inclusiveMatrix());
+	//	cout << "dagpath: " << test1.fullPathName() << endl;
+	//	cout << "Global transform for " << tform.name() << ": " << worldMtrx << endl << endl;
+	//}
 }
 /*
  * how Maya calls this method when a node is added.
@@ -84,7 +95,7 @@ void dirtyNode(MObject& node, void* clientData)
  * new LIGHT    : kTransform, [kPointLight, kDirLight, kAmbientLight]
  * new JOINT    : kJoint
  */
-void nodeAdded(MObject &node, void * clientData) {
+void nodeAdded(MObject& node, void* clientData) {
 	if (node.hasFn(MFn::kDependencyNode)) {
 		MFnDependencyNode dn(node);
 		cout << "Node added: " << dn.name() << endl;
@@ -92,38 +103,43 @@ void nodeAdded(MObject &node, void * clientData) {
 		if (status == MS::kSuccess) {
 			string name = dn.name().asChar();
 			string suffix = "NameChanged";
-			auto itr = callbacks.find(name+suffix);
-			if (itr != callbacks.end()) {
-				cout<< "Callback erased: " + itr->first + "\n";
-				MMessage::removeCallback(itr->second);
-				callbacks.erase(itr);
-			}
-			callbacks.insert({name + suffix, cbID });
-		}
-		else {
-			cout << "Error inserting callback: " + dn.name() + "\n";
-		}
-		cbID = MNodeMessage::addAttributeChangedCallback(node, nodeAttributeChanged, NULL, &status);
-		if (status == MS::kSuccess) {
-			string name = dn.name().asChar();
-			string suffix = "AttributeChanged";
 			auto itr = callbacks.find(name + suffix);
 			if (itr != callbacks.end()) {
 				cout << "Callback erased: " + itr->first + "\n";
 				MMessage::removeCallback(itr->second);
-				callbacks.erase(itr);
+				callbacks[name + suffix] = cbID;
 			}
-			callbacks.insert({ name + suffix, cbID });
+			else {
+				callbacks.insert({ name + suffix, cbID });
+			}
 		}
 		else {
 			cout << "Error inserting callback: " + dn.name() + "\n";
 		}
-		if (node.hasFn(MFn::kTransform)) {
+		if (node.hasFn(MFn::kMesh)) {
 
-			cbID = MNodeMessage::addNodeDirtyCallback(node, dirtyNode, NULL, &status);
+			cbID = MNodeMessage::addAttributeChangedCallback(node, nodeAttributeChanged, NULL, &status);
 			if (status == MS::kSuccess) {
 				string name = dn.name().asChar();
-				string suffix = "Dirty";
+				string suffix = "AttributeChanged";
+				auto itr = callbacks.find(name + suffix);
+				if (itr != callbacks.end()) {
+					cout << "Callback erased: " + itr->first + "\n";
+					MMessage::removeCallback(itr->second);
+					callbacks.erase(itr);
+				}
+				callbacks.insert({ name + suffix, cbID });
+			}
+			else {
+				cout << "Error inserting callback: " + dn.name() + "\n";
+			}
+		}
+		if (node.hasFn(MFn::kTransform)) {
+
+			cbID = MNodeMessage::addAttributeChangedCallback(node, nodeTransformChanged, NULL, &status);
+			if (status == MS::kSuccess) {
+				string name = dn.name().asChar();
+				string suffix = "TransformChanged";
 				auto itr = callbacks.find(name + suffix);
 				if (itr != callbacks.end()) {
 					cout << "Callback erased: " + itr->first + "\n";
@@ -140,8 +156,8 @@ void nodeAdded(MObject &node, void * clientData) {
 }
 
 void timerCallback(float elapsedTime, float lastTime, void* clientData) {
-	//cout << "5 seconds have elapsed\n";
-	//Look it's annoying to have this going while testing other things
+	totaltime += elapsedTime;
+	cout << round(totaltime) << " seconds have elapsed\n";
 }
 
 /*
@@ -160,24 +176,23 @@ EXPORT MStatus initializePlugin(MObject obj) {
 	if (MFAIL(res)) {
 		CHECK_MSTATUS(res);
 		return res;
-	}  
+	}
 
 	// redirect cout to cerr, so that when we do cout goes to cerr
 	// in the maya output window (not the scripting output!)
 	std::cout.set_rdbuf(MStreamUtils::stdOutStream().rdbuf());
 	std::cerr.set_rdbuf(MStreamUtils::stdErrorStream().rdbuf());
 	cout << "Plugin successfully loaded ==============" << endl;
-	
+
 	// register callbacks here for
 	auto nodeAddedId = MDGMessage::addNodeAddedCallback(nodeAdded, "dependNode", NULL, &status);
 	if (status == MS::kSuccess) {
-		callbacks.insert({"nodeAdded", nodeAddedId});
+		callbacks.insert({ "nodeAdded", nodeAddedId });
 	}
 	else {
 		cout << "Error inserting nodeAdded callback\n";
 	}
 
-	// MDGMessage::addNodeRemovedCallback(nodeRemoved, "dependNode", NULL, &status);
 	auto timerID = MTimerMessage::addTimerCallback(5, timerCallback, NULL, &status);
 	if (status == MS::kSuccess) {
 		callbacks.insert({ "timer", timerID });
@@ -192,14 +207,13 @@ EXPORT MStatus initializePlugin(MObject obj) {
 
 	return res;
 }
-	
+
 
 EXPORT MStatus uninitializePlugin(MObject obj) {
 	MFnPlugin plugin(obj);
 
 	cout << "Plugin unloaded =========================" << endl;
 
-	//MMessage::removeCallbacks(callbackIdArray);
 	cout << "Removing callbacks: \n";
 	for (auto i : callbacks) {
 		cout << i.first + '\n';
